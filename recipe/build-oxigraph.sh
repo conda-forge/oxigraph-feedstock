463cc312ac7d553efcf59d0ac8ba0969a50ba337
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-12-13 19:16:05 -0600

    try rust compiler, some maturin flags, jinja for c compilers

--- recipe/build-oxigraph.sh
+++ recipe/build-oxigraph.sh
@@ -4,7 +4,7 @@ set -eux
 export RUST_BACKTRACE=1
 
 export CARGO_HOME="$BUILD_PREFIX/cargo"
-export PATH=$PATH:$CARGO_HOME/bin
+export PATH="${PATH}:${CARGO_HOME}/bin"
 
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
 export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
@@ -12,25 +12,25 @@ export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
 
 rustc --version
 
-mkdir -p $CARGO_HOME
+mkdir -p "${CARGO_HOME}"
 
 if [[ $PKG_NAME == "oxigraph-server" ]]; then
-    cd $SRC_DIR/server
+    cd "${SRC_DIR}/server"
     cargo-bundle-licenses \
         --format yaml \
-        --output ${SRC_DIR}/THIRDPARTY.yml
-    cargo install --root $PREFIX --path .
+        --output "${SRC_DIR}/THIRDPARTY.yml"
+    cargo install --root "${PREFIX}" --path .
 fi
 
 if [[ $PKG_NAME == "pyoxigraph" ]]; then
-    cd $SRC_DIR/python
+    cd "${SRC_DIR}/python"
     cargo-bundle-licenses \
         --format yaml \
-        --output ${SRC_DIR}/THIRDPARTY.yml
-    maturin build --release -i $PYTHON
-    $PYTHON -m pip install $SRC_DIR/target/wheels/*.whl
+        --output "${SRC_DIR}/THIRDPARTY.yml"
+    "${PYTHON}" generate_stubs.py pyoxigraph pyoxigraph.pyi --black
+    maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}"
+    "${PYTHON}" -m pip install "${SRC_DIR}/target/wheels/*.whl"
 fi
-
 
 rm -f "${PREFIX}/.crates.toml"
 rm -f "${PREFIX}/.crates2.json"
