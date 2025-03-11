a0d451b324657aac3c07b876f30f7ec71346a56b
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-03-11 11:09:37 -0500

    fix stubgen

--- recipe/build-stubs.py
+++ recipe/build-stubs.py
@@ -3,7 +3,8 @@ import sys
 from subprocess import call
 from pathlib import Path
 
-SP_DIR = Path(site.getsitepackages()[0]) / "pyoxigraph"
+PKG_NAME = "pyoxigraph"
+SP_DIR = Path(site.getsitepackages()[0]) / PKG_NAME
 SP_STUBS = SP_DIR / "__init__.py"
 SP_TYPED = SP_DIR / "py.typed"
 
@@ -13,7 +14,7 @@ def main() -> int:
     print("... creating", SP_TYPED, flush=True)
     SP_TYPED.touch()
     print("... generating", SP_STUBS, flush=True)
-    return call([sys.executable, "generate_stubs.py", SP_STUBS])
+    return call([sys.executable, "generate_stubs.py", PKG_NAME, SP_STUBS])
 
 
 if __name__ == "__main__":
